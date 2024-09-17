2ee14f0499807379093c420ea157a62a5cb272b9
gbmello <gabriel_meello@hotmail.com>
2024-09-17 10:45:34 -0300

    feat - criando os entities

---
+++ src/Services/Publisher/Domain/MoviesRental.Domain/Entities/Director.cs
@@ -0,0 +1,62 @@
+﻿using MoviesRental.Core.DomainObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace MoviesRental.Domain.Entities
+{
+    public class Director : Entity
+    {
+        protected Director() { }
+
+        public Director(
+            string name,
+            string surname)
+        {
+            UpdateName(name);
+            UpdateSurname(surname);
+
+        }
+
+        public string Name { get; private set; }
+        public string Surname { get; private set; }
+
+        public const int MIN_LENGTH = 3;
+        public const int MAX_LENGHT = 30;
+
+
+        public List<Dvd> _dvds = new List<Dvd>();
+
+        public IReadOnlyList<Dvd> Dvds => _dvds;
+
+        public string FullName()
+        {
+            return $"{Name} {Surname}";
+
+        }
+
+        public void UpdateName(string name)
+        {
+            if (!ValidateName(name))
+                throw new DomainException($"Ivalid name of Director");
+            Name = name;
+        }
+        public void UpdateSurname(string surname)
+        {
+            if (!ValidateName(surname))
+                throw new DomainException(($"Ivalid name of Director"));
+            Surname = surname;
+        }
+
+        private bool ValidateName(string value)
+        {
+            if (string.IsNullOrEmpty(value) || value.Length < MIN_LENGTH || value.Length > MAX_LENGHT) return false;
+
+            return Regex.IsMatch(value, "^(?=.*[A-ZÀ-Ÿ])(?=.*[a-zà-ÿ])[A-Za-zÀ-ÿ]+$");
+
+        }
+    }
+}
---
+++ src/Services/Publisher/Domain/MoviesRental.Domain/Entities/Dvd.cs
@@ -0,0 +1,13 @@
+﻿using MoviesRental.Core.DomainObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoviesRental.Domain.Entities
+{
+    public class Dvd : Entity
+    {
+    }
+}
